5844923ed7b646e1e24adc62e4e49654c0373378
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2024-07-08 01:37:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function shortcut(s1, s2) {
   let shortForm = "";
-	if(s1 == undefined || s2 == undefined){
-	    shortForm += "''";
-	} else {
-		shortForm += "'"+s1[0] + s2[0]+"'"
-	}
-	return shortForm;
+	if (s1 === undefined || s2 === undefined) {
+    return '';
+  } else {
+  return s1[0] + s2[0];
+  }
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
